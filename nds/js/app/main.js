d4d32f1de2aab3a9b40e6c8df31942aa664b7206
oxidase <michael.krasnyk@gmail.com>
2015-11-22 23:20:39 +0100

    Added Mapquest OSM tiles layer

--- nds/js/app/main.js
+++ nds/js/app/main.js
@@ -7,21 +7,24 @@ function (L, $, ko, sidebar, tileid, TileGrid, require) {
 <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a>';
 
     var tiles = [
-        {name: 'Wikimedia', url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', id: '',
+        {name: 'Mapquest OSM', url: 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', id: '', subdomains: '1234',
+         attribution: 'Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>' +
+         ' | Tiles courtesy of <a href="http://www.mapquest.com">MapQuest</a>'},
+        {name: 'Wikimedia', url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', id: '', subdomains: 'abc',
          attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'},
-        {name: 'OSM', url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', id: '',
+        {name: 'OSM', url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', id: '', subdomains: 'abc',
          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 				 'Imagery © <a href="http://mapbox.com">Mapbox</a>'},
-        {name: 'Mapbox', url: 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', id: 'examples.map-zr0njcqy',
+        {name: 'Mapbox', url: 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', id: 'examples.map-zr0njcqy', subdomains: 'abc',
          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 				 'Imagery © <a href="http://mapbox.com">Mapbox</a>'},
-        {name: 'OSM Toner', url: 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', id: '',
+        {name: 'OSM Toner', url: 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', id: '', subdomains: 'abc',
          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'},
-        {name: 'OSM Mapnik', url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', id: '',
+        {name: 'OSM Mapnik', url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', id: '', subdomains: 'abc',
          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'},
-        {name: 'Landscape', url: 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', id: '',
+        {name: 'Landscape', url: 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', id: '', subdomains: 'abc',
          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>' +
          ' Contributors <a href="http://thunderforest.com/">Thunderforest</a>'}
     ];
@@ -30,10 +33,10 @@ function (L, $, ko, sidebar, tileid, TileGrid, require) {
     var baseLayers = {};
     for (var idx in tiles) {
         var t = tiles[idx];
-        baseLayers[t.name] = L.tileLayer(t.url, {attribution: t.attribution + ' | ' + freepik, id: t.id});
+        baseLayers[t.name] = L.tileLayer(t.url, {attribution: t.attribution + ' | ' + freepik, id: t.id, subdomains: t.subdomains});
     }
 
-	  var map = L.map('map', {center: [48.137270, 11.575506], zoom: 15, layers: [baseLayers['Wikimedia']], attributionControl: false});
+	  var map = L.map('map', {center: [48.137270, 11.575506], zoom: 15, layers: [baseLayers['Mapquest OSM']], attributionControl: false});
 
     var tilesGrid = new TileGrid();
     tilesGrid.addTo(map);
