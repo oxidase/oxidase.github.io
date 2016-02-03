674a98c16ff26e565bfaf3bee932267f303d57b3
Michael Krasnyk <michael.krasnyk@gmail.com>
2016-02-04 00:11:35 +0100

    Minor fixes for extreme values at poles

--- nds/js/nds/tilegrid.js
+++ nds/js/nds/tilegrid.js
@@ -18,22 +18,22 @@ function point(lon, lat) {
     lon = tileid.lon2nds(lon);
     lat = tileid.lat2nds(lat);
     lon = lon >= 0x80000000 ? lon - 0x100000000 : lon;
-    lat = (lat >= 0x40000000 ? lat - 0x080000000 : lat);
+    lat = (lat > 0x40000000 ? lat - 0x080000000 : lat);
     return round(lon, 6) + ', ' + round(lat, 6);
 }
 
 function dist(lon1, lat1, lon2, lat2) {
     var m = distance.wgs84(lat1, lon1, lat2, lon2);
     if (!isFinite(m))
-        return m;
+        return '0';
     return (m > 1200) ? round(m / 1000., 2).toString() + ' km' : round(m, 2).toString() + ' m';
 }
 
 
 function tileInfo(id) {
     var coord = tileid.tileid2wgs(id),
-        level = tileid.tileid2level(id),
-        d = tileid.level2distance(level);
+        l = tileid.tileid2level(id),
+        d = tileid.level2distance(l);
 
     return '<svg width="225" height="240" class="info">'
            + '<rect x="20" y="20" width="200" height="200" style="fill:rgba(0,0,255,0.0);stroke-width:3;stroke:rgb(0,0,0)" />'
@@ -44,9 +44,9 @@ function tileInfo(id) {
            + '<circle cx="20" cy="220" r="4" stroke="black" stroke-width="0" fill="red" />'
            + '<text x="25"   y="215" text-anchor="start">' + point(coord[0], coord[1]) + '</text>'
            + '<text x="120"  y="15"  text-anchor="middle">' + dist(coord[0], coord[1]+d, coord[0]+d, coord[1]+d) + '</text>'
-           + '<text x="120"  y="235" text-anchor="middle">' + dist(coord[0], coord[1], coord[0]+d, coord[1]) + '</text>'
-           + '<text x="20"   y="120"  text-anchor="middle" transform="rotate(-90,20,120) translate(0,-5)">' + dist(coord[0], coord[1], coord[0], coord[1]+d) + '</text>'
-           + '<text x="120"  y="160" text-anchor="middle">Tile: ' + id + ', level ' + level + '</text>'
+           + '<text x="120"  y="235" text-anchor="middle">' + dist(coord[0], coord[1], coord[0]+d, coord[1], l) + '</text>'
+           + '<text x="20"   y="120"  text-anchor="middle" transform="rotate(-90,20,120) translate(0,-5)">' + (l > 0 ? dist(coord[0], coord[1], coord[0], coord[1]+d, l) : '20040 km') + '</text>'
+           + '<text x="120"  y="160" text-anchor="middle">Tile: ' + id + ', level ' + l + '</text>'
            + '</svg>'
 }
 
