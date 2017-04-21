ba35167d6fced4639b26e73242f7653e440011d1
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-04-21 10:09:36 +0200

    Load data GeoJSON

--- lighthouses/lib/resize.js
+++ lighthouses/lib/resize.js
@@ -2,7 +2,6 @@
 (function(){
     var attachEvent = document.attachEvent;
     var isIE = navigator.userAgent.match(/Trident/);
-    console.log(isIE);
     var requestFrame = (function(){
         var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
             function(fn){ return window.setTimeout(fn, 20); };
